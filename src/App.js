6216633c986b47c115a2aad4328fa6a0a76d2010
mohammednur1997 <mm887549@gmail.com>
2021-11-24 19:03:21 +0300

    third_milestone

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component, Fragment, useEffect} from 'react';
 import AppRouting from "./Router/AppRouting";
 import {HashRouter} from "react-router-dom";
+import OneSignal from 'react-onesignal';
 
-class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <HashRouter>
-                    <AppRouting/>
-                </HashRouter>
-            </Fragment>
-        );
-    }
+function App() {
+
+    useEffect(() => {
+        OneSignal.init({
+            appId: "9575b204-f235-4427-807b-318fa992606a"
+        });
+    });
+
+    return (
+        <Fragment>
+            <HashRouter>
+                <AppRouting/>
+            </HashRouter>
+        </Fragment>
+    );
 }
 
 export default App;
